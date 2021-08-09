d9e47f200c43aad49efa0ba525a9cdd179a1063c
Dmitry Dimidyuk <havegood95@gmail.com>
2021-08-09 23:44:21 +0300

    added cache properties

--- src/main/kotlin/com/ddimidyuk/moviespace/cache/CaffeineCacheConfig.kt
+++ src/main/kotlin/com/ddimidyuk/moviespace/cache/CaffeineCacheConfig.kt
@@ -1,6 +1,7 @@
 package com.ddimidyuk.moviespace.cache
 
 import com.github.benmanes.caffeine.cache.Caffeine
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.cache.CacheManager
 import org.springframework.cache.annotation.EnableCaching
 import org.springframework.cache.caffeine.CaffeineCacheManager
@@ -12,11 +13,16 @@ import java.util.concurrent.TimeUnit
 @Profile("caffeine")
 @Configuration
 @EnableCaching
-class CaffeineCacheConfig {
+class CaffeineCacheConfig(
+        @Value("\${cache.max-size}") val cacheMaxSize: Long,
+        @Value("\${cache.expire-time-hours}") val expireTimeHours: Long,
+) {
 
     @Bean
     fun caffeineConfig(): Caffeine<Any, Any> {
-        return Caffeine.newBuilder().expireAfterWrite(1, TimeUnit.DAYS)
+        return Caffeine.newBuilder()
+                .maximumSize(cacheMaxSize)
+                .expireAfterWrite(expireTimeHours, TimeUnit.HOURS)
     }
 
     @Bean
